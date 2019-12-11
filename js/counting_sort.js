b002c7a3812d95957b2b65efdd1f43f95bb3f286
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2019-12-11 13:14:16 -0500

    Create rearrange function

--- js/counting_sort.js
+++ js/counting_sort.js
@@ -26,13 +26,30 @@ function countKeysLess(arrayEqual, maximun) {
   return arrayLess;
 }
 
+function rearrange(arrayToSort, arrayLess, maximun) {
+  console.log("Inside rearrange function");
+  console.log("This is a arrayToSort: " + arrayToSort + "\nThis is the maximun value at the arrayToSort: " + maximun + "\nThis is a arrayLess: " + arrayLess);
+
+  let sortedArray = new Array(arrayToSort.length).fill(0);
+  console.log("sortedArray before throughout the array: " + sortedArray);
+
+  for (var i = 0; i < arrayToSort.length; i++) {
+    let key = arrayToSort[i];
+    let index = arrayLess[key];
+    sortedArray[index] = arrayToSort[i];
+    arrayLess[key]++;
+  }
+  console.log("sortedArray after throughout the array: " + sortedArray + "\n\n");
+  return sortedArray;
+}
+
 function countingSort(arrayTosort, maximun) {
   let arrayEqual = countKeysEqual(arrayToSort, maximun);
   let arrayLess = countKeysLess(arrayEqual, maximun);
-  return arrayLess;
+  let sortedArray = rearrange(arrayToSort, arrayLess, maximun);
+  return sortedArray;
 }
 
 let arrayToSort = [4, 1, 5, 0, 1, 6, 5, 1, 5, 3];
 let maximun = 6;
-
 console.log(countingSort(arrayToSort, maximun));
